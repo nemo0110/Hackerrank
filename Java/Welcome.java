09f642e35b458bca3f154b013257c63c1cea41c3
nemo0110 <66428042+nemo0110@users.noreply.github.com>
2023-04-03 14:10:18 +0200

    Create Welcome.java

---
+++ Java/Welcome.java
@@ -0,0 +1,19 @@
+/*
+You must print two lines of output:
+
+Print Hello, World. on the first line.
+Print Hello, Java. on the second line.
+
+Sample Output
+
+Hello, World.
+Hello, Java.
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
